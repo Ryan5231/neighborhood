5b60b2730d68a81706f83c00c4417b69352c7dcd
dbcapprentice <dbcapprentice@dbc.com>
2013-06-13 11:58:54 -0700

    first commit rails skeleton with pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neighborhood::Application.config.secret_token = 'd7318c0159a395a4cd345adbd200b5b8aa1f95f8f4a351715fd828557b91cd567a6ddd487497aac7efc722359154209caba0bc08072a066a00a920efcc074690'
